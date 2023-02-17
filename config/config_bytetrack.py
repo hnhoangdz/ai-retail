85d34abfcdbd466c9dcd0911fb35225694c47043
codewithleo1103 <muaxuan0320@gmail.com>
2023-02-17 17:37:32 +0700

    init config

---
+++ config/config_bytetrack.py
@@ -0,0 +1,30 @@
+
+class COLOR: 
+    red = (255, 0, 0)
+    blue = (0, 0, 255)
+    green = (0, 255, 0)
+    yellow = (255, 255, 0)
+    fuchsia = (255, 0, 255)
+    pink = (241, 167, 228)
+
+classes =  {
+    0:"person",
+    1:"oreo",
+    2:"coffee",
+    3:"chinsu",
+    4:"coca",
+    5:"fanta",
+    6:"hand",
+}
+
+class CLASSES:
+    person = 0
+    oreo = 1
+    coffee = 2
+    chinsu = 3
+    coca = 4
+    fanta = 5
+    hand = 6
+
+class AREA:
+    shelve = ((), ())
