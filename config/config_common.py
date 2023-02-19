2ddd875ba3b95d4bbb92c782261cfc28bb231886
codewithleo1103 <muaxuan0320@gmail.com>
2023-02-20 00:12:32 +0700

    init config

---
+++ config/config_common.py
@@ -0,0 +1,27 @@
+
+class COLOR: 
+    red = (255, 0, 0)
+    blue = (0, 0, 255)
+    green = (0, 255, 0)
+    yellow = (255, 255, 0)
+    fuchsia = (255, 0, 255)
+    pink = (241, 167, 228)
+
+class CLASSES:
+    person = 0
+    oreo = 1
+    coffee = 2
+    chinsu = 3
+    coca = 4
+    fanta = 5
+    hand = 6
+
+object =  {
+    0:"person",
+    1:"oreo",
+    2:"coffee",
+    3:"chinsu",
+    4:"coca",
+    5:"fanta",
+    6:"hand",
+}
---
+++ config/config_mmpose.py
@@ -0,0 +1,10 @@
+class AREA:
+    '''
+    - Area coordinates is the ratio of that point 
+    on the image to the real coordinates
+    - Coordinate formated ratio: ((tl), (tr), (br), (bl))
+    '''
+    shelve = ((0.2, 0.43), (0.39, 0.2), (0.44, 0.4), (0.29, 0.64))
+    attend = ((0.14, 0.5), (0.4, 0.17), (0.73, 0.33), (0.58, 0.97))
+    payment = ((0.73, 0.33), (0.87, 0.4), (0.73, 0.33), (0.58, 0.97))
+
