808fa6dd329989b4aa4424a06c515ab705fe6913
hung.tranmanh <hung.tranmanh@vti.com.vn>
2023-02-28 09:48:36 +0700

    add config parameters

--- cfg.py
+++ cfg.py
@@ -1,18 +1,34 @@
+# shelf_area = [
+#     [0.2 , 0.43], # tl
+#     [0.4 , 0.17], # tr
+#     [0.5 , 0.39], # br
+#     [0.33, 0.68] # bl
+# ]
+
+# attend_area = [
+#     [0.14, 0.5], # tl
+#     [0.4 , 0.17], # tr
+#     [0.73, 0.33], # br
+#     [0.58, 0.97] # bl
+# ]
+
+in_out_area = [
+    [0.8, 0.64],
+    [0.95, 0.98]
+]
+
 shelf_area = [
-    [0.2 , 0.43], # tl
-    [0.4 , 0.17], # tr
-    [0.5 , 0.39], # br
-    [0.33, 0.68] # bl
+    [0.4, 0.124],
+    [0.5, 0.57]
 ]
 
-attend_area = [
-    [0.14, 0.5], # tl
-    [0.4 , 0.17], # tr
-    [0.73, 0.33], # br
-    [0.58, 0.97] # bl
+payment_area = [
+    [0.6, 0.67],
+    [0.75, 0.637],
+    [0.827, 0.863],
+    [0.684, 0.94]
 ]
 
-classes = ["person", "oreo", "coffe", "tuong_ot", "coca", "fanta", "hand"]
 area_colors = {
     "outside": (0,0,255),
     "shelf": (0,255,0),
@@ -21,7 +37,7 @@ area_colors = {
 }
 
 COUNTER = 0
-CONSECUTIVE_FRAME = 10
+CONSECUTIVE_FRAME = 3
 
 class COLOR:
     red = (0,0,255)
@@ -30,14 +46,45 @@ class COLOR:
     yellow = (255,255,0)
     magenta = (255,0,255)
 
-classes = {
-    0:"person",
-    1:"oreo",
-    2:"coffe",
-    3:"tuong_ot",
-    4:"coca",
-    5:"fanta",
-    6:"hand",
-}
+classes = ['dark_noodles', 'g7', 'hand', 'haohao', 'modern', 'nabaty', 'nescafe', 'oreo', 'passiona']
+
+# import cv2
+# from PIL import ImageFont
+
+# TITLE = "---------Receipt----------"
+# THICKNESS_TITLE = 2
+# FONT_TITLE = cv2.FONT_HERSHEY_TRIPLEX
+# COLOR_TITLE = (111, 0, 5)
+# FONT_SCALE=0.7
+
+# GREEN = (0,255,0)
+# BLACK = (0,0,0)
+# FONT = cv2.FONT_HERSHEY_COMPLEX
+# THICKNESS = 1
+# LINE_SPACE = 30
+
+# ORG_STT = (10,60)
+# X_STT = 10
+
+# ORG_RETAIL = (70, 60)
+# X_RETAIL = 70
+
+# ORG_AMOUNT = (260, 60)
+# X_AMOUNT = 260
+
+# ORG_PRICE = (380, 60)
+# X_PRICE = 380
+
+# ORG_TOTAL = (500, 60)
+# X_TOTAL = 500
+
+# ORG_ALL = (530, 60)
+# X_ALL = 540
+
+# IMAGE_SIZE = 640
+# CONF_THRESH = 0.75
+# IOU_THRESH = 0.45
+
+
 
     
